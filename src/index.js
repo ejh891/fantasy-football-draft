3c5ee011ae0017e1b69c09fbda0b0f1aa1fe4bdb
EJ Hammond <ehammond@vistaprint.com>
2017-08-31 13:01:39 -0400

    Switch to js extensions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './redux/store/configureStore';
-import App from './components/app/app.jsx';
+import App from './components/app/app';
 
 const store = configureStore({});
 
